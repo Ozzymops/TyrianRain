45a604924b168d769d3d411e4794d0babc2fb07f
Ozzymops <39087842+Ozzymops@users.noreply.github.com>
2022-03-12 18:08:56 +0100

    Begin of WeaponSwap and Adrenaline code

---
+++ TyrianRain/Modules/Professions/Warrior/WarriorAdrenaline.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using RoR2;
+
+namespace TyrianRain.Modules.Professions.Warrior
+{
+    public class WarriorAdrenaline
+    {
+        private CharacterBody warriorCharacterBody;
+
+        private int adrenaline = 0;
+        private int maxAdrenaline = 30;
+        private int adrenalineThreshold = 10;
+        private float warriorDamageDealtCounter = 0;
+        private float warriorDamageTakenCounter = 0;
+
+        // 1000% of damage dealth = 1 adrenaline
+        public void UpdateWarriorDamageDealtCounter(float damageDealt)
+        {
+            warriorDamageDealtCounter += damageDealt;
+
+            if (warriorDamageDealtCounter > 1000)
+            {
+                warriorDamageDealtCounter -= 1000;
+                UpdateAdrenaline(1);
+            }
+        }
+
+        // 30% of max. HP damage taken = 1 adrenaline
+        public void UpdateWarriorDamageTakenCounter(float damageTaken)
+        {
+            warriorDamageTakenCounter += damageTaken;
+
+            if (warriorDamageTakenCounter > (warriorCharacterBody.maxHealth * 0.3f))
+            {
+                warriorDamageTakenCounter -= (warriorCharacterBody.maxHealth * 0.3f);
+                UpdateAdrenaline(1);
+            }
+        }
+
+        // adrenaline sanity check
+        public void UpdateAdrenaline(int addedAdrenaline)
+        {
+            if (adrenaline < maxAdrenaline)
+            {
+                adrenaline += addedAdrenaline;
+            }
+        }
+
+        // reset adrenaline after burst
+        public void DrainAdrenaline()
+        {
+            adrenaline = 0;
+        }
+
+        // return adrenaline data
+        public int[] GetAdrenalineData()
+        {
+            return new int[] { adrenaline, maxAdrenaline, adrenalineThreshold };
+        }
+    }
+}
---
+++ TyrianRain/Modules/Professions/Warrior/WarriorWeaponSwap.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using RoR2;
+
+namespace TyrianRain.Modules.Professions.Warrior
+{
+    enum WarriorWeapons { Greatsword, Hammer, DualAxe, MaceShield, SwordTorch, DualDagger, Longbow, Rifle }
+
+    public class WarriorWeaponSwap
+    {
+        private CharacterBody warriorCharacterBody;
+        private WarriorAdrenaline adrenalineScript;
+
+        private WarriorWeapons weaponLoadout;
+        private int equippedWeapon = 0;
+
+        private void Update()
+        {
+            // input
+            if (Input.GetKeyDown(KeyCode.Alpha1)) // 1
+            {
+                if (equippedWeapon == 0)
+                {
+                    ExecuteBurst();
+                }
+                else
+                {
+                    SwapWeapon(0);
+                }               
+            }
+
+            if (Input.GetKeyDown(KeyCode.Alpha2)) // 2
+            {
+                if (equippedWeapon == 1)
+                {
+                    ExecuteBurst();
+                }
+                else
+                {
+                    SwapWeapon(1);
+                }
+            }
+
+            if (Input.GetKeyDown(KeyCode.Alpha3)) // 3
+            {
+                if (equippedWeapon == 2)
+                {
+                    ExecuteBurst();
+                }
+                else
+                {
+                    SwapWeapon(2);
+                }
+            }
+        }
+
+        // swap weapon index
+        public void SwapWeapon(int weapon)
+        {
+            equippedWeapon = weapon;
+
+            // skill override code
+        }
+
+        // perform burst attack
+        public void ExecuteBurst()
+        {
+            int[] adrenalineData = adrenalineScript.GetAdrenalineData();
+
+            if (adrenalineData[0] >= adrenalineData[2])
+            {
+                // get adrenaline level
+                int adrenalineLevel = Mathf.FloorToInt(adrenalineData[0] % adrenalineData[2]);
+                
+                // execute skill code
+            }
+        }
+    }
+}
